0b06f2bb7cac717439358237edc4620c7ab8039d
Jess Hsieh <jesshsieh9@gmail.com>
2023-02-20 11:32:38 +0800

    Add files via upload

--- google_map_brand.py
+++ google_map_brand.py
@@ -3,19 +3,16 @@ import pandas as pd
 import numpy as np
 
 st.set_page_config(layout="wide")
+st.sidebar.markdown("# Google Map 品牌輿情")
 
-
-st.sidebar.markdown("# PTT 品牌聲量")
-
-
-title = "PTT 品牌聲量 0101-0131"
+title = "Google Map 品牌輿情 0101-0131"
 st.header(f"{title}")
-data = pd.read_csv("data/ptt_table2023-01-01_2023-01-31.csv")
+data = pd.read_csv("data/gmap_review_brand_2023-01-01_2023-01-31.csv")
 data = data.drop("Unnamed: 0",axis = 1)
 n_rows = len(data)
 n_columns = len(data.columns)
 data.set_index('品牌', inplace=True)
-st.dataframe(data,  height = int(35*(n_rows+1)), width = int(70*(n_columns+1)))
+st.dataframe(data,  height = int(35*(n_rows+1)), width = int(150*(n_columns+1)))
 
 def convert_df(df):
     # IMPORTANT: Cache the conversion to prevent computation on every rerun
@@ -30,4 +27,4 @@ st.download_button(
 )
 
 with st.expander("說明"):
-    st.write("1. 滑鼠點兩下可看到資料格全貌 \n 2. 滑鼠選取部分範圍可以複製貼上到 Libre Office 或是 google sheet \n 3. 可以自行調整欄寬、列高 \n 4. 按住 Ctrl F 可以搜尋資料 \n 5. 右下角可以縮放資料表  \n 6. 點擊欄位名稱可以遞增或遞減排序")
+    st.write("1. 滑鼠點兩下可看到資料格全貌 \n 2. 滑鼠選取部分範圍可以複製貼上到 Libre Office 或是 google sheet \n 3. 可以自行調整欄寬、列高 \n 4. 按住 Ctrl F 可以搜尋資料 \n 5. 右下角可以縮放資料表 \n 6. 點擊欄位名稱可以遞增或遞減排序")
